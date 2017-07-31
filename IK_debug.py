19d75f1dcaf84663e8049d67aaf08268bb794c88
Andrei Ghiuru <aghiuru@users.noreply.github.com>
2017-07-31 23:43:44 +0200

    thetas 1-3

--- IK_debug.py
+++ IK_debug.py
@@ -2,6 +2,7 @@ from sympy import *
 from time import time
 from mpmath import radians
 import tf
+import numpy as np
 
 '''
 Format of test case is [ [[EE position],[EE orientation as quaternions]],[WC location],[joint angles]]
@@ -22,8 +23,43 @@ test_cases = {1:[[[2.16135,-1.42635,1.55109],
                   [0.01735,-0.2179,0.9025,0.371016]],
                   [-1.1669,-0.17989,0.85137],
                   [-2.99,-0.12,0.94,4.06,1.29,-4.12]],
-              4:[],
+              4:[[[2.153,0.0,1.946],
+                  [0,0,0,0]],
+                 [1.85,0,1.94],
+                 [0,0,0,0,0,0]],
               5:[]}
+
+
+def r_x(a):
+    return Matrix([[cos(a), -sin(a), 0],
+                   [sin(a),  cos(a), 0],
+                   [     0,       0, 1]])
+
+#    return Matrix([[cos(a), -sin(a), 0, 0],
+#                   [sin(a),  cos(a), 0, 0],
+#                   [     0,       0, 1, 0],
+#                   [     0,       0, 0, 1]])
+
+def r_y(a):
+    return Matrix([[ cos(a), 0, sin(a)],
+                   [      0, 1,      0],
+                   [-sin(a), 0, cos(a)]])
+
+    # return Matrix([[ cos(a), 0, sin(a), 0],
+    #                [      0, 1,      0, 0],
+    #                [-sin(a), 0, cos(a), 0],
+    #                [      0, 0,      0, 1]])
+
+
+def r_z(a):
+    return Matrix([[ 1,      0,       0],
+                   [ 0, cos(a), -sin(a)],
+                   [ 0, sin(a),  cos(a)]])
+
+    # return Matrix([[ 1,      0,       0, 0],
+    #                [ 0, cos(a), -sin(a), 0],
+    #                [ 0, sin(a),  cos(a), 0],
+    #                [ 0,      0,       0, 1]])
 
 
 def test_code(test_case):
@@ -58,22 +94,114 @@ def test_code(test_case):
 
     req = Pose(comb)
     start_time = time()
-    
+
     ########################################################################################
-    ## 
+    ##
 
     ## Insert IK code here!
-    
-    theta1 = 0
-    theta2 = 0
-    theta3 = 0
+
+    # Extract end-effector position and orientation from request
+    # px,py,pz = end-effector position
+    # roll, pitch, yaw = end-effector orientation
+    px = req.poses[0].position.x
+    py = req.poses[0].position.y
+    pz = req.poses[0].position.z
+
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+        [req.poses[0].orientation.x, req.poses[0].orientation.y,
+            req.poses[0].orientation.z, req.poses[0].orientation.w])
+
+    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+    d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+    a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+    alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
+    s = {
+        alpha0: 0, a0: 0, d1: .75,
+        alpha1: -pi/2, a1: .35, d2: 0, q2: q2 - pi/2,
+        alpha2: 0, a2: 1.25, d3: 0,
+        alpha3: -pi/2, a3: -.054, d4: 1.5,
+        alpha4:  pi/2, a4: 0, d5: 0,
+        alpha5: -pi/2, a5: 0, d6: 0,
+        alpha6: 0, a6: 0, d7: .303, q7: 0
+    }
+
+    T0_1 = Matrix([[cos(q1), -sin(q1), 0, a0],
+                   [sin(q1) * cos(alpha0), cos(q1) * cos(alpha0), -sin(alpha0), -sin(alpha0) * d1],
+                   [sin(q1) * cos(alpha0), cos(q1) * sin(alpha0), cos(alpha0), cos(alpha0) * d1],
+                   [0, 0, 0, 1]])
+
+    T1_2 = Matrix([[cos(q2), -sin(q2), 0, a1],
+                   [sin(q2) * cos(alpha1), cos(q2) * cos(alpha1), -sin(alpha1), -sin(alpha1) * d2],
+                   [sin(q2) * cos(alpha1), cos(q2) * sin(alpha1), cos(alpha1), cos(alpha1) * d2],
+                   [0, 0, 0, 1]])
+
+    T2_3 = Matrix([[cos(q3), -sin(q3), 0, a2],
+                   [sin(q3) * cos(alpha2), cos(q3) * cos(alpha2), -sin(alpha2), -sin(alpha2) * d3],
+                   [sin(q3) * cos(alpha2), cos(q3) * sin(alpha2), cos(alpha2), cos(alpha2) * d3],
+                   [0, 0, 0, 1]])
+
+    T3_4 = Matrix([[cos(q4), -sin(q4), 0, a3],
+                   [sin(q4) * cos(alpha3), cos(q4) * cos(alpha3), -sin(alpha3), -sin(alpha3) * d4],
+                   [sin(q4) * cos(alpha3), cos(q4) * sin(alpha3), cos(alpha3), cos(alpha3) * d4],
+                   [0, 0, 0, 1]])
+
+
+    R_total = r_x(yaw) * r_y(pitch) * r_z(roll)
+    R_corr = r_y(+np.pi/2) * r_x(-np.pi)
+
+    p = Matrix([[px], [py], [pz]])
+    n = Matrix([[0], [0], [d7]])
+
+    wc = simplify(p - R_total * R_corr * n).subs(s)
+
+    p_wc = (float(wc[0]), float(wc[1]), float(wc[2]))
+
+    print('WC: {}'.format(wc))
+
+    theta1 = simplify(atan2(wc[1], wc[0])).subs(s)
+    print('theta1: ', theta1)
+
+    # Find O2
+    o2 = (cos(theta1) * a1, sin(theta1) * a1, d1)
+    print('O2: ', o2)
+
+    d_xy = sqrt((wc[0] - o2[0])**2 + (wc[1] - o2[1])**2)
+    print('d_xy: ', d_xy)
+
+    theta_21 = atan2(wc[2] - o2[2], d_xy)
+    print('theta_21: ', theta_21)
+
+    c = sqrt((wc[2] - o2[2])**2 + d_xy**2)
+    print('c: ', c)
+
+    b = sqrt(a3**2 + d4**2)
+
+    theta_22 = acos((c**2 + a2**2 - b**2) / (2 * c * a2))
+    print('theta_22: ', theta_22)
+
+    # Find O3
+    o3z = o2[2] + sin(theta_21 + theta_22) * a2
+    print('o3z: ', o3z)
+
+    theta2 = (pi / 2 - theta_21 - theta_22).subs(s)
+    print('theta2: ', theta2)
+
+    theta31 = atan2(a3, d4)
+    theta32 = acos((b**2 + a2**2 - c**2) / (2 * a2 * b))
+    print('theta31: ', theta31)
+    print('theta32: ', theta32)
+
+    theta3 = (pi / 2 + theta31 - theta32).subs(s)
+    print('theta3: ', theta3)
+
     theta4 = 0
     theta5 = 0
     theta6 = 0
 
-    ## 
+    ##
     ########################################################################################
-    
+
     ########################################################################################
     ## For additional debugging add your forward kinematics here. Use your previously calculated thetas
     ## as the input and output the position of your end effector as your_ee = [x,y,z]
@@ -84,7 +212,7 @@ def test_code(test_case):
     ########################################################################################
 
     ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
-    your_wc = [1,1,1] # <--- Load your calculated WC values in this array
+    your_wc = wc # <--- Load your calculated WC values in this array
     your_ee = [1,1,1] # <--- Load your calculated end effector value from your forward kinematics
     ########################################################################################
 
